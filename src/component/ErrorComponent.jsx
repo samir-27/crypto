99c46b6d1b9a1f3427dc44fd9111fad88c8126b9
samir-27 <sumrasamir27@gmail.com>
2023-09-05 08:16:41 +0530

    add Error component

--- src/component/ErrorComponent.jsx
+++ src/component/ErrorComponent.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Alert, AlertIcon } from "@chakra-ui/react";
+import React from "react";
 
-const ErrorComponent = ({message}) => {
+const ErrorComponent = ({ message }) => {
   return (
-    <div>
+    <Alert
+      status="error"
+      position={"fixed"}
+      bottom={"4"}
+      left={"50%"}
+      transform={"translateX(-50%)"}
+      w={"container.lg"}
+    >
+      <AlertIcon />
       {message}
-    </div>
-  )
-}
+    </Alert>
+  );
+};
 
-export default ErrorComponent
+export default ErrorComponent;
